22843fae9e2fa28860c0f8ce4afd162ff951971c
koosunmyung <hb841@naver.com>
2024-12-03 08:18:07 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -946,9 +946,12 @@ if authentication_status:
 
 
             st.dataframe(df_all_bs_약식,use_container_width=True)
-    sc_t_2 = f"{streamlit_js_eval(js_expressions='screen.width', key = 'SCR')}"
-    if int(sc_t_2) < 1500:
+    # sc_t_2 = f"{streamlit_js_eval(js_expressions='screen.width', key = 'SCR')}"
+    # sc_t = f"{streamlit_js_eval(js_expressions='screen.width', key1 = 'SCR')}"
+    
+    if int(sc_t) < 1500:
         st.text("준비중")
+        st.text(sc_t)
         tab1, tab2, tab3, tab4, tab5 = st.tabs(['🏳 DASHBOARD', '🏳 PL_Graph','🏳 PL', '🏳 PL trend', '🏳 B/S'])
         with tab1:
             df_all_bs = df_all[df_all['손익구분'] == "BS"]
@@ -1181,59 +1184,59 @@ if authentication_status:
     # </style>''', unsafe_allow_html=True)
 
 
-    st.write('''<style>
-    [data-testid="stMarkdownContainer"] {
-        width: fit-content;
-        margin: auto;
-    }
+        st.write('''<style>
+        [data-testid="stMarkdownContainer"] {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stElementContainer"] > div {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stElementContainer"] > div {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stMetricValue"] label {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stMetricValue"] label {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stHeadingWithActionElements"] label {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stHeadingWithActionElements"] label {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stMarkdownContainer"] p {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stMarkdownContainer"] p {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stMarkdownContainer"] label {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stMarkdownContainer"] label {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stHeadingWithActionElements"] div {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stHeadingWithActionElements"] div {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stVerticalBlockBorderWrapper"] label {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stVerticalBlockBorderWrapper"] label {
+            width: fit-content;
+            margin: auto;
+        }
 
-    [data-testid="stVerticalBlockBorderWrapper"] div {
-        width: fit-content;
-        margin: auto;
-    }
+        [data-testid="stVerticalBlockBorderWrapper"] div {
+            width: fit-content;
+            margin: auto;
+        }
 
-    # [data-testid="stMetricValue"] div {
-    #     width: fit-content;
-    #     margin: auto;
-    # }
+        # [data-testid="stMetricValue"] div {
+        #     width: fit-content;
+        #     margin: auto;
+        # }
 
-    </style>''', unsafe_allow_html=True)
-             
+        </style>''', unsafe_allow_html=True)
+                
 
 # I usually dump any scripts at the bottom of the page to avoid adding unwanted blank lines
 # st.markdown(f'<style>{css}</style>',unsafe_allow_html=True)
