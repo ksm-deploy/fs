f697426541ad05065bbd5fe6f869d993a4f3882e
koosunmyung <hb841@naver.com>
2024-12-03 08:46:12 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1179,7 +1179,7 @@ if authentication_status:
         [data-testid="column"] {
             width: calc(33.3333% - 1rem) !important;
             flex: 1 1 calc(33.3333% - 1rem) !important;
-            min-width: calc(33% - 1rem) !important;
+            min-width: calc(33.3333% - 1rem) !important;
         }
         </style>''', unsafe_allow_html=True)
 
