64b9c99bc1d549711e30651e61f780d3d4f9a6a1
koosunmyung <hb841@naver.com>
2024-12-03 12:15:32 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -954,7 +954,7 @@ if authentication_status:
         # st.text("준비중")
         # st.text(sc_t)
     
-        st.text("------")    
+        
         tab1, tab2, tab3, tab4, tab5 = st.tabs(['🏳 DASHBOARD', '🏳 PL_Graph','🏳 PL', '🏳 PL trend', '🏳 B/S'])
         with tab1:
             df_all_bs = df_all[df_all['손익구분'] == "BS"]
@@ -1108,6 +1108,7 @@ if authentication_status:
             st.dataframe(cfdata,use_container_width=True)
             st.text("")
             # https://github.com/streamlit/streamlit/issues/5003
+            st.write("------")    
             col1, col2, col3 = st.columns(3)
                 # st.text("11")
             with col1:
