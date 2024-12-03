f5574a9e0dc5ab48996d7a59194a9267e87c1cbe
koosunmyung <hb841@naver.com>
2024-12-04 08:12:34 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1318,7 +1318,7 @@ if authentication_status:
 
         # fig.layout.xaxis.fixedrange = True
         # fig.layout.yaxis.fixedrange = False
-        fig.update_layout(dragmode = 'scroll')
+        fig.update_layout(dragmode = 'drag')
         fig.update_layout(xaxis_rangeslider_visible = False)
         st.plotly_chart(fig)
 # I usually dump any scripts at the bottom of the page to avoid adding unwanted blank lines
