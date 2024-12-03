fd8bae75ebd7db2adda9a4605c6dd9bec24c2419
koosunmyung <hb841@naver.com>
2024-12-03 15:21:07 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1308,9 +1308,9 @@ if authentication_status:
         )
         fig.update_yaxes(showticklabels=False)
         fig.update_layout(
-        dragmode = "turntable",
+        dragmode = "pan",
         # ['orbit', 'turntable', 'zoom', 'pan', False]
-
+        # https://jeunna.tistory.com/107
         xaxis = dict(
         tickfont = dict(size=15)),
         title_font_size = 25)
