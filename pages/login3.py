c147c20395acb526c091c7b44352794cf6a40356
koosunmyung <hb841@naver.com>
2024-12-07 08:47:40 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1362,7 +1362,17 @@ if authentication_status:
                 # df_손익_전체_누계['일자'] = pd.to_datetime(df_손익_전체_누계['회계연도'].astype(str) + df_손익_전체_누계['전기월'].astype(str), format='%Y%m')
                 # df_손익_전체_누계['일자'] = pd.PeriodIndex(pd.to_datetime(df_손익_전체_누계[['회계연도','전기월']].assign(day=1)),freq='M')
 
-                df_손익_전체_누계['일자'] = pd.to_datetime(df_손익_전체_누계[['회계연도','전기월']].str.assign(day=1)).dt.to_period('M')
+                # df_손익_전체_누계['일자'] = pd.to_datetime(df_손익_전체_누계[['회계연도','전기월']].str.assign(day=1)).dt.to_period('M')
+
+
+
+                df_손익_전체_누계['일자'] = pd.to_datetime(
+                    
+                    year = df_손익_전체_누계['회계연도']
+                    month = df_손익_전체_누계['전기월'].
+                    freq='M')
+
+
 
                 # 년월까지만 보이게 컬럼변경
                 st.dataframe(df_손익_전체_누계, use_container_width=True)
