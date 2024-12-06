906c40981d70fcb2adfff3673d6e03e165e0a829
koosunmyung <hb841@naver.com>
2024-12-07 08:59:27 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1386,7 +1386,8 @@ if authentication_status:
                 # 멀티셀렉트 
                 # 대상항목 = st.multiselect("대상항목선택",df_손익_전체_누계['중분류'].unique(),default=[])
                 # 단순셀렉트
-                대상항목 = st.selectbox("CHOICE DEVICE",df_손익_전체_누계['중분류'].unique(), index= None)
+                default_ix = '매출'
+                대상항목 = st.selectbox("항목선택",df_손익_전체_누계['중분류'].unique(), index= default_ix)
                 #멀티셀렉트 데이터프레임 연동
                 # df_손익_전체_누계_trand = df_손익_전체_누계[df_손익_전체_누계['중분류'].isin(대상항목)]
                 df_손익_전체_누계_trand = df_손익_전체_누계[df_손익_전체_누계['중분류'] ==대상항목]
