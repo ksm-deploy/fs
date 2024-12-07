9ac69fbe445686cb75f0115d71430c471caf3152
koosunmyung <hb841@naver.com>
2024-12-08 05:43:20 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1500,7 +1500,7 @@ if authentication_status:
             df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.drop(columns='중분류') 
             
             
-            
+            df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.reindex('세분류')
             # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.set_index('세분류')
             df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.style.applymap(
                         lambda x: f"background-color: gray; ", subset = (df_all_bs_약식_누계_병합[df_all_bs_약식_누계_병합['bs분류'] ==""].index,slice(None))
@@ -1509,7 +1509,7 @@ if authentication_status:
                     ).format(precision=0, thousands=',')
             # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.drop
             # df_all_bs_약식_누계_병합.style.hide_index()
-            df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.reindex('세분류')
+            # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.reindex('세분류')
             # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.style.hide(axis='index')
             # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합['세분류':]
             st.dataframe(df_all_bs_약식_누계_병합,use_container_width=True)
