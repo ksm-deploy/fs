e71f06ac0cfaf832eb04bddb8a970d031225f241
koosunmyung <hb841@naver.com>
2024-12-08 05:44:11 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1499,8 +1499,8 @@ if authentication_status:
             # st.dataframe(df_all_bs_약식_누계_병합,use_container_width=True)
             df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.drop(columns='중분류') 
             
-            
-            df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.reindex('세분류')
+            df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.reset_index()
+            df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.set_index('세분류')
             # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.set_index('세분류')
             df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.style.applymap(
                         lambda x: f"background-color: gray; ", subset = (df_all_bs_약식_누계_병합[df_all_bs_약식_누계_병합['bs분류'] ==""].index,slice(None))
