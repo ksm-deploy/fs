c168b944df72abbca26f7c3ad676e4bdb060ed8e
koosunmyung <hb841@naver.com>
2024-12-08 06:04:09 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1513,16 +1513,15 @@ if authentication_status:
             
 
             # 조건부 전체행 컬러변경2
-            # def highlight_survived(s):
-            #     return ['background-color: green']*len(s) if s.bs분류 else ['background-color: red']*len(s)
+            def highlight_survived(s):
+                return ['background-color: green']*len(s) if s.bs분류 else ['background-color: red']*len(s)
+            st.dataframe(df_all_bs_약식_누계_병합.style.apply(highlight_survived, axis=0))
 
-            def color_survived(val):
-                color = 'green' if val else 'red'
-                return f'background-color: {color}'
-            st.dataframe(df_all_bs_약식_누계_병합.style.applymap(color_survived, subset=['bs분류']))
-
-            # st.dataframe(df_all_bs_약식_누계_병합.style.apply(highlight_survived, axis=1))
-
+            # def color_survived(val):
+            #     color = 'green' if val else 'red'
+            #     return f'background-color: {color}'
+            
+            # st.dataframe(df_all_bs_약식_누계_병합.style.applymap(color_survived, subset=['bs분류']))
 
                         # lambda x: f"background-color: gray; ", subset = (df_all_bs_약식_누계_병합[df_all_bs_약식_누계_병합['bs분류'] ==""].index)
                         # lambda _: "background-color: gray; ", subset=(['bs중분류','영업이익'], slice(None))
