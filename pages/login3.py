3a82b60f4dcfd46ddeeab64184339638151ae0c1
koosunmyung <hb841@naver.com>
2024-12-07 13:15:51 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1498,7 +1498,7 @@ if authentication_status:
             df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.style.applymap(
                         lambda x: f"background-color: gray; ", subset = (df_all_bs_약식_누계_병합_서식대상[df_all_bs_약식_누계_병합_서식대상['bs분류'] ==""].index,slice(None))
                         # lambda _: "background-color: gray; ", subset=(['bs중분류','영업이익'], slice(None))
-                    ).format(precision=0, thousands=',').hide_index()
+                    ).format(precision=0, thousands=',').hide_index().render()
             # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.drop
             # df_all_bs_약식_누계_병합.style.hide_index()
             # df_all_bs_약식_누계_병합 = df_all_bs_약식_누계_병합.set_index('중분류')
