c72bb2e6736e5aa22bfdade0981abc21081aaf0f
koosunmyung <hb841@naver.com>
2024-12-08 06:15:06 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -74,8 +74,8 @@ if authentication_status == None:
     st.error("please enter your name and pw")
 if authentication_status:
     # st.header("hellow")
-    cols = st.columns(30)
-    with cols[29]:
+    cols = st.columns(60)
+    with cols[59]:
         authenticator.logout("logout","main")    
 
     @st.cache_data
