a75b798382a2cbd01fe8f6b19847ad39275e1558
koosunmyung <hb841@naver.com>
2024-12-07 09:39:41 +0900

    t

--- pages/login3.py
+++ pages/login3.py
@@ -1361,11 +1361,15 @@ if authentication_status:
                 #일자 컬럼 생성 - 타입일자
                 # https://www.marsja.se/combine-year-and-month-columns-in-pandas/
                 df_손익_전체_누계['일자'] = pd.to_datetime(df_손익_전체_누계['회계연도'].astype(str) + df_손익_전체_누계['전기월'].astype(str), format='%Y%m')
+                st.dataframe(df_손익_전체_누계, use_container_width=True)
+                
                 # df_손익_전체_누계['일자'] = pd.PeriodIndex(pd.to_datetime(df_손익_전체_누계[['회계연도','전기월']].assign(day=1)),freq='M')
 
                 # df_손익_전체_누계['일자'] = pd.to_datetime(df_손익_전체_누계[['회계연도','전기월']].str.assign(day=1)).dt.to_period('M')
 
                 df_손익_전체_누계['일자'] = df_손익_전체_누계['일자'].astype(str).dt.strftime("%Y/%m")
+                st.dataframe(df_손익_전체_누계, use_container_width=True)
+                
                 # df_손익_전체_누계['일자'].strftime('%Y-%m-%d')
 
 
