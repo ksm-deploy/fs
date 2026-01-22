31df9b95a0f657e65a5cec54c62e149a28663661
koosunmyung <hb841@naver.com>
2026-01-23 07:52:25 +0900

    t

--- pages/voices_present.py
+++ pages/voices_present.py
@@ -4,11 +4,8 @@ from urllib.parse import quote
 import pandas as pd
 import streamlit as st
 
-texts_l = []
-
-idx = 0
 def get_posts(query):
-    df_test = pd.DataFrame()
+    texts_l = []
     
     url_query = quote(query)
     url = 'https://search.naver.com/search.naver?ssc=tab.blog.all&sm=tab_jum&query=' + url_query
@@ -20,41 +17,12 @@ def get_posts(query):
     bloggers = blog_tt.find_all('li',{'class':'bx'})
     for n in bloggers:
         bloger = n.find('div',{'class':'view_wrap'}).find('div',{'class':'user_box_inner'}).get_text()
-        st.text(bloger)
-            # st.text(title)
         texts_l.append(bloger)
-        # df_test.loc[idx] = [bloger,0]
-        # idx += 1
-        # st.text(texts_l)
-    df_test = pd.DataFrame(data=texts_l,columns=['text'], index=None)
-
-    # df_test = pd.DataFrame({"test":texts_l})
-    # return df_test
-    return st.dataframe(df_test, hide_index=True, use_container_width=True)
+    
+    df_test = pd.DataFrame(data=texts_l, columns=['text'], index=None)
+    st.dataframe(df_test, hide_index=True, use_container_width=True)
 
 
 
 get_posts('이수안컴퓨터연구소')
-# st.dataframe(df_test,use_container_width=True)
-
-# st.dataframe(df_test,use_container_width=True)
-
-
-
-
-
-    # for _ in range(0):
-
-#     search_url = urllib.request.urlopen(url).read()
-#     soup = BeautifulSoup(search_url, 'html.parser')
-#     posts = soup.find_all('ul',{'class':'lst_view _fe_view_infinite_scroll_append_target'})
-
-#     for post in posts:
-#         title = post.find('li',{'class':'bx'}).find('div',{'class':'view_wrap'}).find('div',{'class':'user_box_inner'}).get_text()
-
-#             # st.text(title)
-#     return st.text(title)
-
-
-# get_posts('이수안컴퓨터연구소')
 
